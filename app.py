bcd2e9e8fea5f5c605e57c3e9fe8939106cbdaeb
Kelly Shull <kelly.t.shull@gmail.com>
2020-08-31 13:31:03 -0700

    app.py opening route

---
+++ app.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+# use &lt; and &gt; as place holders for < > 
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"<a href='/api/v1.0/precipitation'>Precipitation</a><br/>"
+        f"<a href='/api/v1.0/stations'>Stations</a><br/>"
+        f"<a href='/api/v1.0/tobs'>Temperature</a><br/>"
+        f"<a href='/api/v1.0/&lt;start&gt;'>Start Date</a><br/>"
+        f"<a href='/api/v1.0/&lt;start&gt;/&lt;end&gt;>End Date</a><br/>"
+    )
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
